aac3b4d2d641f31ae8f72b2b03b4f1a47370236b
mac <mac@macs-Air.hsd1.wa.comcast.net>
2015-10-23 15:07:23 -0700

    comment out get item

--- client/javascript/controllers/reservations_controller.js
+++ client/javascript/controllers/reservations_controller.js
@@ -1,17 +1,17 @@
 myApp.controller('ReservationsController', function ($scope, $location, $routeParams, ReservationFactory, UserFactory) {
 	
 	// when user opens up vendor and strain, runs this get automatically
-	ReservationFactory.getItem($routeParams.vendor_id, $routeParams.strain_id, function (item){
-		console.log(item);
-		$scope.name = item[0].vendor_name;
-		$scope.vendorID = item[0].vendor_id;
-		$scope.strainName = item[0].strain_name;
-		$scope.imgSRC = item[0].fullsize_img1;
-		$scope.desc = item[0].description;
-		$scope.price_gram = item[0].price_gram;
-		$scope.category = item[0].category;
-		$scope.strainID = item[0].strain_id;
-	});
+	// ReservationFactory.getItem($routeParams.vendor_id, $routeParams.strain_id, function (item){
+	// 	console.log(item);
+	// 	$scope.name = item[0].vendor_name;
+	// 	$scope.vendorID = item[0].vendor_id;
+	// 	$scope.strainName = item[0].strain_name;
+	// 	$scope.imgSRC = item[0].fullsize_img1;
+	// 	$scope.desc = item[0].description;
+	// 	$scope.price_gram = item[0].price_gram;
+	// 	$scope.category = item[0].category;
+	// 	$scope.strainID = item[0].strain_id;
+	// });
 
 	UserFactory.returnUser(function(data){
 			userID = data;
